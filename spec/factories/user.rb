ecc74b1fade166423bcd78f14bf0ac1cb9f23619
Emre Demir <emmredemmir@gmail.com>
2021-04-06 01:02:15 +0300

    Test hatasını düzelt

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { Faker::Internet.unique.email }
+    email { Faker::Internet.unique.email }
     password { '123456' }
     password_confirmation { '123456' }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { build(:user) }
+
   it 'has a valid factory' do
-    expect(build(:user)).to be_valid
+    expect(user).to be_valid
   end
 
   context 'validations' do
