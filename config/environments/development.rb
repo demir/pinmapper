eff9fec97389f1ce7d08118a0575b8379e21676c
Emre Demir <emmredemmir@gmail.com>
2021-04-05 15:35:05 +0300

    Development için mail ayarlarını yap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.noreply_mail[:username],
+    password: Rails.application.credentials.noreply_mail[:password],
+    address: Rails.application.credentials.noreply_mail[:smtp_address],
+    port: '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
