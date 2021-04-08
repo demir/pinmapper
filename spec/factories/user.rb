9abbe0830bd168cdc2fc640c3037172c90a3d9e6
Emre Demir <emmredemmir@gmail.com>
2021-04-08 14:47:56 +0300

    User factory'ye confirmed trait'i ekle

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,11 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { '123456' }
     password_confirmation { '123456' }
+
+    trait :confirmed do
+      confirmed_at { Time.zone.now }
+      confirmation_sent_at { Time.zone.now }
+      confirmation_token { srand.to_s.last(5) }
+    end
   end
 end
