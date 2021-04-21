c11959d6a63c30d830a32a110ae46d997ef8410d
Emre Demir <emmredemmir@gmail.com>
2021-04-22 01:19:15 +0300

    Pin ekranlarını ekle

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'pages#index'
     devise_for :users
+    resources :pins
   end
 end
