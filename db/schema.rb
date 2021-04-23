74928412698be3daa6a8a173b4b35fb2bba278e8
Emre Demir <emmredemmir@gmail.com>
2021-04-23 23:51:11 +0300

    Pin tablosuna latitude longitude index'i ekle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_202034) do
+ActiveRecord::Schema.define(version: 2021_04_23_204949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_04_18_202034) do
     t.text "cover_image_description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["latitude", "longitude"], name: "index_pins_on_latitude_and_longitude"
   end
 
   create_table "users", force: :cascade do |t|
