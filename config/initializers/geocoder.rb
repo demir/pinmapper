2b0a3c3526ecb877e990f7953e9b89733c263bdd
Emre Demir <emmredemmir@gmail.com>
2021-04-23 23:49:27 +0300

    Geocoder konfigürasyonunu geliştir Cache'i aktif et (redis) Varsayılan uzaklık birimini km yap

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,15 +7,16 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: Rails.application.credentials[:google_maps_api_key] # API key for geocoding service
+  api_key: Rails.application.credentials[:google_maps_api_key], # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+  cache:   Redis.new,
+  # cache_prefix: 'geocoder:', # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and Timeout::Error
   # always_raise: [],
   # Calculation options
-  # units: :mi,                 # :km for kilometers or :mi for miles
+  units:   :km # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
