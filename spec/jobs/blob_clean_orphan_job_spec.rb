662500fbddcccde71439a6934b184883aedc40f9
Emre Demir <emmredemmir@gmail.com>
2021-05-10 21:39:51 +0300

    BlobCleanOrphanJob için spec ekle

--- spec/jobs/blob_clean_orphan_job_spec.rb
+++ spec/jobs/blob_clean_orphan_job_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe BlobCleanOrphanJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform_later' do
+    it 'cleans orphan blob' do
+      ActiveJob::Base.queue_adapter = :test
+      expect do
+        BlobCleanOrphanJob.set(wait: 1.day).perform_later(1)
+      end.to have_enqueued_job
+    end
+  end
 end
