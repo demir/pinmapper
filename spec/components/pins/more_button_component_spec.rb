6772343f01958c2a3957ecf53709847c5c039f9d
Emre Demir <emmredemmir@gmail.com>
2021-05-10 18:41:59 +0300

    Pins::MoreButtonComponent spec'ini güncelle

--- spec/components/pins/more_button_component_spec.rb
+++ spec/components/pins/more_button_component_spec.rb
@@ -1,13 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Pins::MoreButtonComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, :confirmed) }
+  let(:pin) { create(:pin, user: user) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  context 'with current_user' do
+    it 'renders more button' do
+      render_inline(described_class.new(pin: pin, current_user: user))
+      expect(rendered_component).to have_css '.pin-more .bi-three-dots'
+    end
+
+    it 'renders edit actions if the owner of pin' do
+      render_inline(described_class.new(pin: pin, current_user: user))
+      expect(rendered_component).to have_css '.pin-more .dropdown-menu a', text: I18n.t('edit')
+      expect(rendered_component).to have_css '.pin-more .dropdown-menu a', text: I18n.t('destroy')
+    end
+
+    it 'is not renders edit actions if not the owner of pin' do
+      different_user = create(:user, :confirmed)
+      render_inline(described_class.new(pin: pin, current_user: different_user))
+      expect(rendered_component).not_to have_css '.pin-more .dropdown-menu a', text: I18n.t('edit')
+      expect(rendered_component).not_to have_css '.pin-more .dropdown-menu a', text: I18n.t('destroy')
+    end
+  end
+
+  context 'without current_user' do
+    it 'is not renders more button' do
+      render_inline(described_class.new(pin: pin))
+      expect(rendered_component).not_to have_css '.pin-more'
+    end
+  end
 end
