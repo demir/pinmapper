193d38c6768bd2a519ca2b021e5095df58c9a346
Emre Demir <emmredemmir@gmail.com>
2021-05-10 18:40:00 +0300

    Componenetlere currrent_user'ı parametre olarak ekle helpers.current_user component içinde nil oluyor test sırasında. Bu yüzden argüman olarak gönderiyorum component'e

--- app/components/pins/pin_component.rb
+++ app/components/pins/pin_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Pins::PinComponent < ViewComponent::Base
-  def initialize(pin:)
+  def initialize(pin:, current_user: nil)
     @pin = pin
+    @current_user = current_user
   end
 end
