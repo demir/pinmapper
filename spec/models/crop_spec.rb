a54eb3916d5f6eb0f4750f9fb1b4d7e55314f7df
Emre Demir <emmredemmir@gmail.com>
2021-05-11 02:41:08 +0300

    Crop model spec'ini düzenle

--- spec/models/crop_spec.rb
+++ spec/models/crop_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Crop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:crop) { build(:crop) }
+
+  it 'has a valid factory' do
+    expect(crop).to be_valid
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:cropable) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:crop_x) }
+    it { is_expected.to validate_presence_of(:crop_y) }
+    it { is_expected.to validate_presence_of(:crop_width) }
+    it { is_expected.to validate_presence_of(:crop_height) }
+  end
 end
