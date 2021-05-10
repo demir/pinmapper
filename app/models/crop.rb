704fa0bcb92db649cfc785a59a8936c70f53bfd3
Emre Demir <emmredemmir@gmail.com>
2021-05-11 02:39:43 +0300

    Crop modeline gerekli validasyonları ekle

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -1,3 +1,10 @@
 class Crop < ApplicationRecord
+  # relations
   belongs_to :cropable, polymorphic: true
+
+  # validations
+  validates :crop_x, presence: true
+  validates :crop_y, presence: true
+  validates :crop_width, presence: true
+  validates :crop_height, presence: true
 end
