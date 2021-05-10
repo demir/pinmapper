cb62f19f1c09ada45308e166c30a456341f6e56d
Emre Demir <emmredemmir@gmail.com>
2021-05-10 03:52:58 +0300

    Pin factory'yi düzenle

--- spec/factories/pins.rb
+++ spec/factories/pins.rb
@@ -1,10 +1,16 @@
 FactoryBot.define do
   factory :pin do
-    name { "MyString" }
-    address { "MyString" }
-    latitude { "9.99" }
-    longitude { "9.99" }
-    privacy { 1 }
-    cover_image_description { "MyText" }
+    name { Faker::Address.city }
+    address { 'Samsun, Türkiye' }
+    privacy { 'public' }
+    cover_image_description { Faker::Lorem.paragraph }
+    association :user, :confirmed
+    tag_list { Faker::Lorem.words(number: 3).join(',') }
+    description { Faker::Lorem.paragraph }
+
+    trait :invalid do
+      address { 'FooBoo' }
+      tag_list { Faker::Lorem.words(number: 6).join(',') }
+    end
   end
 end
