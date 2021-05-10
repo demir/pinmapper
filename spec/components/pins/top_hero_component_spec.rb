695157ecb4ad8baa962976853cda968b60019f1c
Emre Demir <emmredemmir@gmail.com>
2021-05-10 18:42:53 +0300

    Pins::TopHeroComponent spec'ini güncelle

--- spec/components/pins/top_hero_component_spec.rb
+++ spec/components/pins/top_hero_component_spec.rb
@@ -1,13 +1,11 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Pins::TopHeroComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'renders background image which has a hero_in class' do
+    expect(
+      render_inline(described_class.new(title: I18n.t('pins.pin'))).css('.animate__fadeInUp').to_html
+    ).to include(
+      I18n.t('pins.pin')
+    )
+  end
 end
