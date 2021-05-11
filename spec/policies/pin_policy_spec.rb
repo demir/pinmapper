8e95c6dc808e69ae0acba053b3dc482a3a80ad78
Emre Demir <emmredemmir@gmail.com>
2021-05-11 04:14:49 +0300

    Pin poliçe spec'ini düzenle

--- spec/policies/pin_policy_spec.rb
+++ spec/policies/pin_policy_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PinPolicy, type: :policy do
-  let(:user) { User.new }
-
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, :confirmed) }
+  let(:pin) { create(:pin, user: user) }
+
+  permissions :create?, :new? do
+    it 'denies access without current_user' do
+      expect(subject).not_to permit(nil, pin)
+    end
+
+    it 'grants access with current_user' do
+      expect(subject).to permit(user, pin)
+    end
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  permissions :update?, :edit?, :destroy? do
+    it 'denies access if not the owner of pin' do
+      different_user = create(:user, :confirmed)
+      expect(subject).not_to permit(different_user, pin)
+    end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'grants access if the owner of pin' do
+      expect(subject).to permit(user, pin)
+    end
   end
 end
