cb53152cbf0324c80829c06d43683a4c9af1f614
Emre Demir <emmredemmir@gmail.com>
2021-09-13 00:55:08 +0300

    theia-sticky-sidebar'ı kaldır

--- app/javascript/src/theme/main.js
+++ app/javascript/src/theme/main.js
@@ -1,5 +1,3 @@
-import "theia-sticky-sidebar/js/theia-sticky-sidebar";
-
 (function ($) {
 
   "use strict";
@@ -8,25 +6,6 @@ import "theia-sticky-sidebar/js/theia-sticky-sidebar";
     $('.hero_in h1,.hero_in form').addClass('animate__animated');
     $('.hero_single, .hero_in').addClass('start_bg_zoom');
     $(window).scroll();
-
-    // Sticky nav
-    $(window).on('scroll', function () {
-      if ($(this).scrollTop() > 1) {
-        $('.header').addClass("sticky");
-      } else {
-        $('.header').removeClass("sticky");
-      }
-    });
-
-    // Sticky sidebar
-    $('#sidebar').theiaStickySidebar({
-      additionalMarginTop: 150
-    });
-
-    // Sticky titles
-    $('.fixed_title').theiaStickySidebar({
-      additionalMarginTop: 180
-    });
 
     // Mobile Menu
     const hamburger = document.getElementById("hamburger");
