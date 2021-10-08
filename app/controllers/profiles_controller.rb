344fcae5f199249e9ba14c6e5bab419505914330
Emre Demir <emmredemmir@gmail.com>
2021-10-08 16:07:37 +0300

    WIP: Followers ekranı için infinite scroll özelliği ekle

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ProfilesController < ApplicationController
+  include Pagy::Backend
+  helper Pagy::Frontend
   before_action :set_user
   def show; end
 
@@ -18,7 +20,13 @@ class ProfilesController < ApplicationController
     end
   end
 
-  def followers; end
+  def followers
+    @pagy, @followers = pagy @user.followers
+    respond_to do |f|
+      f.turbo_stream
+      f.html
+    end
+  end
 
   def following; end
 
---
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["scrollArea", "pagination"]
+
+  connect() {
+    this.createObserver()
+  }
+  createObserver() {
+    const observer = new IntersectionObserver(
+      entries => this.handleIntersect(entries),
+      {
+        // https://github.com/w3c/IntersectionObserver/issues/124#issuecomment-476026505
+        threshold: [0, 1.0],
+      }
+    )
+    observer.observe(this.scrollAreaTarget)
+  }
+  handleIntersect(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.loadMore()
+      }
+    })
+  }
+  loadMore() {
+    const next = this.paginationTarget.querySelector(".page.next > a")
+    if (!next) {
+      return
+    }
+    const href = next.href
+    fetch(href, {
+      headers: {
+        Accept: "text/vnd.turbo-stream.html",
+      },
+    })
+      .then(r => r.text())
+      .then(html => Turbo.renderStreamMessage(html))
+  }
+}
