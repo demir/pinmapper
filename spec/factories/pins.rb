e4ac134a81475a81aaf17a93d06fbf68edfaafc8
Emre Demir <emmredemmir@gmail.com>
2021-10-20 02:32:14 +0300

    Pin factory etiketleri unique olsun

--- spec/factories/pins.rb
+++ spec/factories/pins.rb
@@ -7,12 +7,12 @@ FactoryBot.define do
     privacy { 'public' }
     cover_image_description { Faker::Lorem.paragraph }
     association :user, :confirmed
-    tag_list { Faker::Lorem.words(number: 3).join(',') }
+    tag_list { Faker::Lorem.unique.words(number: 3).join(',') }
     description { Faker::Lorem.paragraph }
 
     trait :invalid do
       address { 'FooBooFooFFFooBooFooFF' }
-      tag_list { Faker::Lorem.words(number: 6).join(',') }
+      tag_list { Faker::Lorem.unique.words(number: 6).join(',') }
     end
 
     trait :cover_image_crop do
