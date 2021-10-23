6894ac5c984ea6306c0e76ef210b2acdc148d30e
Emre Demir <emmredemmir@gmail.com>
2021-10-23 22:48:49 +0300

    Panoya ekle dropdown'daki görüntüleme hatalarını düzelt

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -9,9 +9,29 @@ export default class extends Controller {
     }
   }
 
-  button_mouseout() {
-    if (!this.element.classList.contains('show')) {
-      this.element.classList.add('show')
+  hide() {
+    let dropdowns = this.element.getElementsByClassName('add-pin-to-board-dropdown');
+    for (let dropdownParent of dropdowns) {
+      let dropdown = dropdownParent.querySelector('.add-pin-to-board')
+      if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
+        this.toggle_added(dropdownParent);
+      }
+    }
+  }
+
+  toggle_added(dropdownParent) {
+    let dropdownButton = dropdownParent.querySelector('.add_to_board_btn')
+    let dropdownMenu = dropdownParent.querySelector('.add-pin-to-board')
+    let removeButtons = dropdownMenu.querySelectorAll('.body > .board-list-item-for-pin .remove-button')
+    if (removeButtons.length > 0) {
+      if (!dropdownButton.classList.contains('added')) {
+        dropdownButton.classList.add('added')
+      }
+    } else {
+      if (dropdownButton.classList.contains('added')) {
+        dropdownButton.classList.remove('added')
+      }
     }
   }
 }
