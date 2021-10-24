91e1a06312873a7c558010cf7a0be464398827f2
Emre Demir <emmredemmir@gmail.com>
2021-10-24 23:05:01 +0300

    Panoya ekle dropdown her açıldığında scroll en üstte olsun

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -13,6 +13,7 @@ export default class extends Controller {
     let dropdowns = this.element.getElementsByClassName('add-pin-to-board-dropdown');
     for (let dropdownParent of dropdowns) {
       let dropdown = dropdownParent.querySelector('.add-pin-to-board');
+      dropdownParent.querySelector('.body').scrollTop = 0;
       if (dropdown.classList.contains('show')) {
         dropdown.classList.remove('show');
         let input = dropdownParent.querySelector('form');
