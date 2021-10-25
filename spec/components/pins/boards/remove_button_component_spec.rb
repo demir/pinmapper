9244887d1ac89ef190d7d9418f2be67bf3ab89cf
Emre Demir <emmredemmir@gmail.com>
2021-10-25 22:07:33 +0300

    Pins::Boards::RemoveButtonComponent spec'lerini ekle

--- spec/components/pins/boards/remove_button_component_spec.rb
+++ spec/components/pins/boards/remove_button_component_spec.rb
@@ -3,13 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe Pins::Boards::RemoveButtonComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:current_user) { create(:user, :confirmed) }
+  let(:board) { create(:board) }
+  let(:pin) { create(:pin) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  context 'with current_user' do
+    context 'when not added to board' do
+      it 'do not show button' do
+        render_inline(described_class.new(board: board, pin: pin, current_user: current_user))
+        expect(rendered_component).not_to have_css '.btn_1'
+      end
+    end
+
+    context 'when added to board' do
+      before do
+        board.pins << pin
+        render_inline(described_class.new(board: board, pin: pin, current_user: current_user))
+      end
+
+      it 'shows remove button with added text' do
+        expect(rendered_component).to have_css '.btn_1', text: I18n.t('added')
+      end
+    end
+  end
+
+  context 'without current_user' do
+    before do
+      render_inline(described_class.new(board: board, pin: pin, current_user: nil))
+    end
+
+    it 'do not show button' do
+      expect(rendered_component).not_to have_css '.btn_1'
+    end
+  end
 end
