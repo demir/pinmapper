f0c73c9eac76861adf94df9c7bb28df3d11f6851
Emre Demir <emmredemmir@gmail.com>
2021-10-26 02:39:51 +0300

    Pins::Boards::AddToBoardButtonComponent için gerekli spec'leri ekle

--- spec/components/pins/boards/add_to_board_button_component_spec.rb
+++ spec/components/pins/boards/add_to_board_button_component_spec.rb
@@ -3,13 +3,135 @@
 require 'rails_helper'
 
 RSpec.describe Pins::Boards::AddToBoardButtonComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:current_user) { create(:user, :confirmed) }
+  let(:pin) { create(:pin) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    render_inline(described_class.new(pin: pin, current_user: current_user))
+  end
+
+  describe 'dropdown div' do
+    it 'id' do
+      expect(rendered_component).to have_css "div[id='add-pin-to-board-dropdown_pin_#{pin.id}']"
+    end
+
+    it 'controller' do
+      expect(rendered_component).to have_css 'div[data-controller="dropdown"]'
+    end
+
+    it 'classes' do
+      expect(rendered_component).to have_css 'div[class="dropdown dropleft add-pin-to-board-dropdown"]'
+    end
+
+    it 'controller id value' do
+      expect(rendered_component).to have_css "div[data-dropdown-id-value='add-pin-to-board-dropdown_pin_#{pin.id}']"
+    end
+  end
+
+  describe 'dropdown-menu button' do
+    it 'id' do
+      expect(rendered_component).to have_css 'a[id="add-pin-to-board-menu"]'
+    end
+
+    context 'if added any pin to board' do
+      let(:board) { create(:board, user: current_user) }
+
+      before do
+        board.pins << pin
+        render_inline(described_class.new(pin: pin, current_user: current_user))
+      end
+
+      it 'class' do
+        expect(rendered_component).to have_css 'a[class="add_to_board_btn added"]'
+      end
+    end
+
+    context 'if there is no added pin to board' do
+      it 'class' do
+        expect(rendered_component).to have_css 'a[class="add_to_board_btn"]'
+      end
+    end
+  end
+
+  describe 'dropdown-menu div' do
+    it 'class' do
+      expect(rendered_component).to have_css 'div[class="dropdown-menu add-pin-to-board"]'
+    end
+
+    it 'data-action' do
+      expect(rendered_component).to(
+        have_css('div[data-action="click->dropdown#stopPropagation turbo:frame-load->dropdown#updateButton"]')
+      )
+    end
+  end
+
+  describe 'header' do
+    it 'dropdown header text' do
+      expect(rendered_component).to have_css '.header span', text: I18n.t('boards.add_to_board')
+    end
+  end
+
+  context 'when there is board' do
+    before do
+      current_user.boards << create(:board)
+      render_inline(described_class.new(pin: pin, current_user: current_user))
+    end
+
+    describe 'header' do
+      it 'renders search form' do
+        expect(rendered_component).to have_css '.header .search-form'
+      end
+    end
+
+    describe 'body' do
+      it 'boards header text' do
+        expect(rendered_component).to have_css '.body span', text: I18n.t('boards.boards')
+      end
+
+      context 'turbo frame' do
+        it 'id' do
+          expect(rendered_component).to have_css "turbo-frame[id='add_to_board_list_body_pin_#{pin.id}']"
+        end
+
+        it 'src' do
+          expect(rendered_component).to(
+            have_css("turbo-frame[src='#{add_to_board_list_boards_path(pin_id: pin, locale: I18n.locale)}']")
+          )
+        end
+
+        it 'loading' do
+          expect(rendered_component).to have_css 'turbo-frame[loading="lazy"]'
+        end
+
+        it 'spinner' do
+          expect(rendered_component).to have_css '.body .justify-content-center .spinner-border.pinmapper.m-4'
+        end
+      end
+    end
+  end
+
+  context 'when there is no board' do
+    describe 'header' do
+      it 'not renders search form' do
+        expect(rendered_component).not_to have_css '.header .search-form'
+      end
+    end
+
+    describe 'body' do
+      it 'information message' do
+        expect(rendered_component).to have_css '.no-boards span', text: I18n.t('boards.there_is_no_boards')
+      end
+
+      context 'create new board button' do
+        it 'class' do
+          expect(rendered_component).to have_css '.no-boards .btn_1.outline.full-width'
+        end
+
+        it 'link' do
+          expect(rendered_component).to have_css ".no-boards a[href='#{new_board_path(locale: I18n.locale)}']",
+                                                 text: I18n.t('boards.index.create_new_board')
+        end
+      end
+    end
+  end
 end
