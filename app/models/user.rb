2aa0f9f32f6e4a2d9ba0ce043b77398ae1810ee2
Emre Demir <emmredemmir@gmail.com>
2021-11-03 00:36:03 +0300

    E-posta ya da kullanıcı adı ile giriş yapma özelliği ekle

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   acts_as_voter
+  attr_writer :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
@@ -22,4 +24,30 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_many :user_boards, dependent: :destroy
   has_many :following_boards, through: :user_boards, source: :board, dependent: :destroy
+
+  # validations
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  validates :username, format: { with: /^[a-z0-9_.]*$/, multiline: true }
+  validate :validate_username
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    if login
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  private
+
+  def validate_username
+    errors.add(:username, :invalid) if User.exists?(email: username)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_234535) do
+ActiveRecord::Schema.define(version: 2021_11_02_193042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -166,9 +166,11 @@ ActiveRecord::Schema.define(version: 2021_10_31_234535) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
