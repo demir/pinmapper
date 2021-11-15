66784ada4e5f583381ce88717bd3764b0172b986
Emre Demir <emmredemmir@gmail.com>
2021-11-16 01:44:58 +0300

    PG için unaccent eklentisini aktif et

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_193858) do
+ActiveRecord::Schema.define(version: 2021_11_15_222352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
