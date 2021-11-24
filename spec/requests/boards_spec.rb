51088d73698d2f622369df7afabb8e5643fdea73
Emre Demir <emmredemmir@gmail.com>
2021-11-24 23:43:52 +0300

    Request speclerine HTTP_ACCEPT_LANGUAGE header'ı ekle

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -17,6 +17,7 @@ require 'rails_helper'
 RSpec.describe '/boards', type: :request do
   let(:current_user) { create(:user, :confirmed) }
   let(:board) { create(:board, user: current_user) }
+  let(:headers) { { 'HTTP_ACCEPT_LANGUAGE' => 'en-US,en;q=0.9' } }
 
   context 'with current_user' do
     before do
@@ -25,33 +26,33 @@ RSpec.describe '/boards', type: :request do
 
     describe 'GET /index' do
       it 'renders a successful response' do
-        get boards_url
+        get boards_url, headers: headers
         expect(response).to be_successful
       end
     end
 
     it 'GET /following_boards' do
-      get following_boards_boards_path
+      get following_boards_boards_path, headers: headers
       expect(response).to be_successful
     end
 
     describe 'GET /show' do
       it 'renders a successful response' do
-        get board_url(id: board)
+        get board_url(id: board), headers: headers
         expect(response).to be_successful
       end
     end
 
     describe 'GET /new' do
       it 'renders a successful response' do
-        get new_board_url
+        get new_board_url, headers: headers
         expect(response).to be_successful
       end
     end
 
     describe 'GET /edit' do
       it 'render a successful response' do
-        get edit_board_url(id: board)
+        get edit_board_url(id: board), headers: headers
         expect(response).to be_successful
       end
     end
@@ -60,12 +61,12 @@ RSpec.describe '/boards', type: :request do
       context 'with valid parameters' do
         it 'creates a new Board' do
           expect do
-            post boards_url, params: { board: attributes_for(:board) }
+            post boards_url, params: { board: attributes_for(:board) }, headers: headers
           end.to change(Board, :count).by(1)
         end
 
         it 'redirects to the created board' do
-          post boards_url, params: { board: attributes_for(:board) }
+          post boards_url, params: { board: attributes_for(:board) }, headers: headers
           expect(response).to redirect_to(boards_url)
         end
       end
@@ -73,12 +74,12 @@ RSpec.describe '/boards', type: :request do
       context 'with invalid parameters' do
         it 'does not create a new Board' do
           expect do
-            post boards_url, params: { board: attributes_for(:board, :invalid) }
+            post boards_url, params: { board: attributes_for(:board, :invalid) }, headers: headers
           end.to change(Board, :count).by(0)
         end
 
         it 'renders new template' do
-          post boards_url, params: { board: attributes_for(:board, :invalid) }
+          post boards_url, params: { board: attributes_for(:board, :invalid) }, headers: headers
           expect(response).to have_http_status(:unprocessable_entity)
           assert_select 'div[class="alert alert-danger"]',
                         text: I18n.t('simple_form.error_notification.default_message')
@@ -89,13 +90,13 @@ RSpec.describe '/boards', type: :request do
     describe 'PATCH /update' do
       context 'with valid parameters' do
         it 'updates the requested board' do
-          patch board_url(id: board), params: { board: attributes_for(:board, name: 'Pinmapper!!') }
+          patch board_url(id: board), params: { board: attributes_for(:board, name: 'Pinmapper!!') }, headers: headers
           board.reload
           expect(board.name).to match('Pinmapper!!')
         end
 
         it 'redirects to the index' do
-          patch board_url(id: board), params: { board: attributes_for(:board, name: 'Pinmapper!!') }
+          patch board_url(id: board), params: { board: attributes_for(:board, name: 'Pinmapper!!') }, headers: headers
           board.reload
           expect(response).to redirect_to(boards_url)
         end
@@ -103,7 +104,7 @@ RSpec.describe '/boards', type: :request do
 
       context 'with invalid parameters' do
         it 'renders a successful response' do
-          patch board_url(id: board), params: { board: attributes_for(:board, :invalid) }
+          patch board_url(id: board), params: { board: attributes_for(:board, :invalid) }, headers: headers
           assert_select 'div[class="alert alert-danger"]',
                         text: I18n.t('simple_form.error_notification.default_message')
         end
@@ -114,13 +115,13 @@ RSpec.describe '/boards', type: :request do
       it 'destroys the requested board' do
         new_board = create(:board, user: current_user)
         expect do
-          delete board_url(id: new_board)
+          delete board_url(id: new_board), headers: headers
         end.to change(Board, :count).by(-1)
       end
 
       it 'redirects to the boards list' do
         new_board = create(:board, user: current_user)
-        delete board_url(id: new_board)
+        delete board_url(id: new_board), headers: headers
         expect(response).to redirect_to(boards_url)
       end
     end
@@ -130,7 +131,7 @@ RSpec.describe '/boards', type: :request do
 
       describe '#add_to_board_list' do
         it 'render a successful response' do
-          get add_to_board_list_boards_path(pin_id: pin)
+          get add_to_board_list_boards_path(pin_id: pin), headers: headers
           expect(response).to be_successful
         end
       end
@@ -142,24 +143,24 @@ RSpec.describe '/boards', type: :request do
 
         it 'raises validation error #add_pin' do
           expect  do
-            get add_pin_board_path(id: board, pin_id: pin)
+            get add_pin_board_path(id: board, pin_id: pin), headers: headers
           end.to raise_error(ActiveRecord::RecordInvalid)
         end
 
         it 'render a successful response #remove_pin' do
-          get remove_pin_board_path(id: board, pin_id: pin)
+          get remove_pin_board_path(id: board, pin_id: pin), headers: headers
           expect(response).to be_successful
         end
       end
 
       context 'not added to board' do
         it 'render a successful response #add_pin' do
-          get add_pin_board_path(id: board, pin_id: pin)
+          get add_pin_board_path(id: board, pin_id: pin), headers: headers
           expect(response).to be_successful
         end
 
         it 'render a successful response #remove_pin' do
-          get remove_pin_board_path(id: board, pin_id: pin)
+          get remove_pin_board_path(id: board, pin_id: pin), headers: headers
           expect(response).to be_successful
         end
       end
@@ -169,7 +170,7 @@ RSpec.describe '/boards', type: :request do
   context 'without current_user' do
     describe 'GET /index' do
       it 'can not renders a successful response' do
-        get boards_url
+        get boards_url, headers: headers
         expect(response).not_to be_successful
       end
     end
@@ -178,14 +179,14 @@ RSpec.describe '/boards', type: :request do
       context 'secret board' do
         it 'can not renders a successful response' do
           new_board = create(:board, privacy: 'secret')
-          get board_url(id: new_board)
+          get board_url(id: new_board), headers: headers
           expect(response).not_to be_successful
         end
       end
 
       context 'public board' do
         it 'renders a successful response' do
-          get board_url(id: board)
+          get board_url(id: board), headers: headers
           expect(response).to be_successful
         end
       end
@@ -193,26 +194,26 @@ RSpec.describe '/boards', type: :request do
 
     describe 'GET /new' do
       it 'can not renders a successful response' do
-        get new_board_url
+        get new_board_url, headers: headers
         expect(response).not_to be_successful
       end
     end
 
     describe 'GET /edit' do
       it 'can not render a successful response' do
-        get edit_board_url(id: board)
+        get edit_board_url(id: board), headers: headers
         expect(response).not_to be_successful
       end
     end
 
     it 'can not POST /create' do
       expect do
-        post boards_url, params: { board: attributes_for(:board) }
+        post boards_url, params: { board: attributes_for(:board) }, headers: headers
       end.to change(Board, :count).by(0)
     end
 
     it 'can not PATCH /update' do
-      patch board_url(id: board), params: { board: attributes_for(:board, name: 'Pinmapper!!') }
+      patch board_url(id: board), params: { board: attributes_for(:board, name: 'Pinmapper!!') }, headers: headers
       board.reload
       expect(board.name).not_to match('Pinmapper!!')
     end
@@ -220,7 +221,7 @@ RSpec.describe '/boards', type: :request do
     it 'can not DELETE /destroy' do
       new_board = create(:board, user: current_user)
       expect do
-        delete board_url(id: new_board)
+        delete board_url(id: new_board), headers: headers
       end.to change(Board, :count).by(0)
     end
 
@@ -229,28 +230,28 @@ RSpec.describe '/boards', type: :request do
 
       describe '#add_to_board_list' do
         it 'not renders a successful response' do
-          get add_to_board_list_boards_path(pin_id: pin)
+          get add_to_board_list_boards_path(pin_id: pin), headers: headers
           expect(response).not_to be_successful
         end
       end
 
       describe '#add_pin' do
         it 'not renders a successful response' do
-          get add_pin_board_path(id: board, pin_id: pin)
+          get add_pin_board_path(id: board, pin_id: pin), headers: headers
           expect(response).not_to be_successful
         end
       end
 
       describe '#remove_pin' do
         it 'not renders a successful response' do
-          get remove_pin_board_path(id: board, pin_id: pin)
+          get remove_pin_board_path(id: board, pin_id: pin), headers: headers
           expect(response).not_to be_successful
         end
       end
     end
 
     it 'can not GET /following_boards' do
-      get following_boards_boards_path
+      get following_boards_boards_path, headers: headers
       expect(response).not_to be_successful
     end
   end
