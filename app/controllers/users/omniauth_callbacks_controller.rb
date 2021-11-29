c7c9a7504eda73372d817f76f777a5dc1e89554d
Emre Demir <emmredemmir@gmail.com>
2021-11-30 00:21:39 +0300

    Rubocop düzeltmelerini yap

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,12 +7,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user&.persisted?
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    # rubocop:disable Metrics/AbcSize
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url
     end
-  end
+    # rubocop:enable Metrics/AbcSize
 
   def failure
     redirect_to root_path
