c05225d9b616856aa8ab8ecb5281483c68d2d399
Emre Demir <emmredemmir@gmail.com>
2021-12-18 00:24:58 +0300

    Pano için frindly_id kullanma

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class Board < ApplicationRecord
-  extend FriendlyId
   include ActiveRecord::Searchable
   include TranslateEnum
-
-  # friendly_id
-  friendly_id :name, use: :slugged
 
   # scopes
   pg_search_scope :trigram_search_by_name,
