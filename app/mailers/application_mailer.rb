6f27da89b3fed11bd8f3b8eaa4bc4a868c9eb943
Emre Demir <emmredemmir@gmail.com>
2021-12-28 01:57:39 +0300

    Mail ayarlarını yap

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "'Pinmapper' <#{Rails.application.credentials.noreply_mail[:username]}>"
   layout 'mailer'
 end
