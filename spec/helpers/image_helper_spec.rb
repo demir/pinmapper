452f0762dc2f70cadb1de328c1c142a8989d66e6
Emre Demir <emmredemmir@gmail.com>
2022-01-27 02:10:59 +0300

    Kırpılan imajların kırpma boyutlarındaki taşma sorununu düzelt

--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -4,15 +4,20 @@ require 'rails_helper'
 
 RSpec.describe ImageHelper, type: :helper do
   let(:crop_constraints) { { crop: [3, 55, 19, 24] } }
+  let(:metadata) { { 'identified' => true, 'width' => 100, 'height' => 100, 'analyzed' => true } }
 
   it 'thumbor_crop' do
-    expect(helper.thumbor_crop(crop_constraints)).to eq('3x55:19x24')
+    expect(helper.thumbor_crop(crop_constraints, metadata)).to eq('3x55:19x24')
+  end
+
+  it 'thumbor_crop with wrong constraints' do
+    expect(helper.thumbor_crop({ crop: [3, 155, 219, 24] }, metadata)).to eq('3x100:100x24')
   end
 
   it 'cloudfront_src' do
     cloud_front_url = Rails.application.credentials.dig(:aws, :cloud_front, :api_endpoint)
     expect(helper.cloudfront_src(filters: '400x400/filters:quality(40)',
-                                 thumbor_crop: helper.thumbor_crop(crop_constraints), image_key: '18493')).to(
+                                 thumbor_crop: helper.thumbor_crop(crop_constraints, metadata), image_key: '18493')).to(
                                    include("#{cloud_front_url}/3x55:19x24/400x400/filters:quality(40)/18493?Expires=")
                                  )
   end
