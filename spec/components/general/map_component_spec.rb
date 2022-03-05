357940798b06d6da1419dafe97f3e0975565b119
Emre Demir <emmredemmir@gmail.com>
2022-03-05 15:05:45 +0300

    General::MapComponent speclerini ekle

--- spec/components/general/map_component_spec.rb
+++ spec/components/general/map_component_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe General::MapComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:pins_array) { create_list(:pin, 2) }
+  let(:pins) { Pin.where(id: pins_array.pluck(:id)) }
+  let(:marker_service) { MapServices::GenerateMarkers::Pin.call(pins) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    render_inline(described_class.new(markers: marker_service[:payload]))
+  end
+
+  context 'div.map' do
+    it 'data-controller' do
+      expect(rendered_component).to have_css 'div[data-controller="map"]'
+    end
+
+    it 'data-makers-value' do
+      expect(rendered_component).to have_css 'div[data-map-markers-value]'
+    end
+
+    it 'data-access-token-value' do
+      expect(rendered_component).to(
+        have_css("div[data-map-access-token-value='#{Rails.application.credentials.dig(:mapbox, :access_token)}']")
+      )
+    end
+
+    it 'data-target-map-container' do
+      expect(rendered_component).to(
+        have_css('div[data-target="map.container"]')
+      )
+    end
+  end
 end
