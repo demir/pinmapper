09aa3160233a3f1a5e3a2de0dcd07162ebcbb2e6
Emre Demir <emmredemmir@gmail.com>
2022-03-16 13:21:14 +0300

    Location ekranı ekle

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -2,9 +2,18 @@
 
 class ExploreController < ApplicationController
   include Pagy::Backend
+  before_action :set_pin, only: %i[location]
 
   def index
-    @pagy, @pins = pagy_pins_latest Pin, items: 10
+    @pagy, @pins = pagy_pins_latest items: 10
+    respond_to do |f|
+      f.html
+      f.turbo_stream
+    end
+  end
+
+  def location
+    @pagy, @pins = pagy_nearby_pins @pin, items: 10
     respond_to do |f|
       f.html
       f.turbo_stream
@@ -13,8 +22,17 @@ class ExploreController < ApplicationController
 
   private
 
-  def pagy_pins_latest(collection, vars = {})
-    pagy = Pagy.new(count: collection.count(:all), page: params[:page], **vars)
+  def set_pin
+    @pin = Pin.find_by(token: params[:token])
+  end
+
+  def pagy_pins_latest(vars = {})
+    pagy = Pagy::Countless.new(page: params[:page], countless_minimal: true, **vars)
     [pagy, Pins::Latest.call(user: nil, pagy: pagy)]
+  end
+
+  def pagy_nearby_pins(pin, vars = {})
+    pagy = Pagy::Countless.new(page: params[:page], countless_minimal: true, **vars)
+    [pagy, Pins::Latest.nearby_pins(pin, pagy)]
   end
 end
