69e381158d40b818af70cf2e17da965419b433bd
Emre Demir <emmredemmir@gmail.com>
2022-04-01 14:33:22 +0300

    Farklı domainler için app_url helper'ı oluştur

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def app_url(path = nil)
+    domain = Rails.application.credentials.domain
+    return domain if path.nil?
+
+    "#{domain}/#{path}"
+  end
 end
