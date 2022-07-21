6d2a223884edccaf02c00c0c952b2f9238817f8f
Emre Demir <emmredemmir@gmail.com>
2022-07-21 19:41:29 +0300

    PhotoSwipe'a caption özelliği ekle

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -13,6 +13,35 @@ export default class extends Controller {
       pswpModule: PhotoSwipe
     };
     const lightbox = new PhotoSwipeLightbox(options);
+    lightbox.on('uiRegister', function () {
+      lightbox.pswp.ui.registerElement({
+        name: 'custom-caption',
+        order: 9,
+        isButton: false,
+        appendTo: 'root',
+        html: '',
+        onInit: (el, pswp) => {
+          lightbox.pswp.on('change', () => {
+            const currSlideElement = lightbox.pswp.currSlide.data.element;
+            let captionHTML = '';
+            if (currSlideElement) {
+              const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+              if (hiddenCaption) {
+                captionHTML = hiddenCaption.innerHTML;
+              } else {
+                captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+              }
+            }
+            if (captionHTML) {
+              el.style.visibility = 'visible';
+              el.innerHTML = captionHTML;
+            } else {
+              el.style.visibility = 'hidden';
+            }
+          });
+        }
+      });
+    });
     lightbox.init()
   }
 }
