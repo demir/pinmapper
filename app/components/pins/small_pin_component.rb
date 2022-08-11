66e43a7da622be0ad45a1549485543de607ee27d
Emre Demir <emmredemmir@gmail.com>
2022-08-11 17:40:34 +0300

    Sürükle sırala özelliğini iyileştir

--- app/components/pins/small_pin_component.rb
+++ app/components/pins/small_pin_component.rb
@@ -3,18 +3,19 @@
 module Pins
   class SmallPinComponent < ViewComponent::Base
     with_collection_parameter :pin
-    attr_reader :pin, :current_user, :board
+    attr_reader :pin, :current_user, :board, :drag_sort
 
-    def initialize(pin:, current_user:, board: nil)
+    def initialize(pin:, current_user:, board: nil, drag_sort: false)
       @pin = pin
       @current_user = current_user
       @board = board
+      @drag_sort = drag_sort
     end
 
     private
 
     def move_pin_url
-      return '' if board.blank? || pin.blank?
+      return '' if board.blank? || pin.blank? || drag_sort.blank? || current_user.blank?
 
       move_pin_board_path(board, pin.id)
     end
